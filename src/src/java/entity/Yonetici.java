a4140a109656fdea07f6f21f37a1f10ca8ca6856
oguzhan2027 <oguzhancelik2027@gmail.com>
2022-04-08 12:47:42 +0300

    entity.yonetici eklendi

---
+++ src/src/java/entity/Yonetici.java
@@ -0,0 +1,54 @@
+package entity;
+
+public class Yonetici {
+
+    private int id;
+    private String adSoyad;
+
+    public Yonetici() {
+    }
+
+    public Yonetici(int id, String adSoyad) {
+        this.id = id;
+        this.adSoyad = adSoyad;
+    }
+
+    public String getAdSoyad() {
+        return adSoyad;
+    }
+
+    public void setAdSoyad(String adSoyad) {
+        this.adSoyad = adSoyad;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 23 * hash + this.id;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Yonetici other = (Yonetici) obj;
+        return this.id == other.id;
+    }
+
+}
