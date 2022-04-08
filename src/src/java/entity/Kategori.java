bf38e786a5b89ea4a1a6391605f5e42df089ee65
nesli9 <doanneslihan@gmail.com>
2022-04-08 13:59:43 +0300

    entity kategori aşaması yapıldı

---
+++ src/src/java/entity/Kategori.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entity;
+
+
+public class Kategori {
+    private int id;
+    private String kategoriAd;
+
+    public Kategori() {
+    }
+
+    public Kategori(int id, String kategoriAd) {
+        this.id = id;
+        this.kategoriAd = kategoriAd;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getKategoriAd() {
+        return kategoriAd;
+    }
+
+    public void setKategoriAd(String kategoriAd) {
+        this.kategoriAd = kategoriAd;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 29 * hash + this.id;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Kategori other = (Kategori) obj;
+        return this.id == other.id;
+    }
+    
+    
+}
