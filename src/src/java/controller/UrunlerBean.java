d61c12035363aaa32907f25a91a429e092155217
tugcebilici <tugcebilici123@gmail.com>
2022-05-27 15:34:21 +0300

    urunler bean

--- src/src/java/controller/UrunlerBean.java
+++ src/src/java/controller/UrunlerBean.java
@@ -21,11 +21,55 @@ public class UrunlerBean implements Serializable {
 
     public UrunlerBean() {
     }
-  public String getTitle(int id) {
+    private int page = 1;
+    private int pageSize = 2;
+    private int pageCount;
+
+    public void next() {
+        if (this.page == this.getPageCount()) {
+            this.page = 1;
+        } else {
+            this.page++;
+        }
+    }
+
+    public void previous() {
+        if (this.page == 1) {
+            this.page = this.getPageCount();
+        } else {
+            this.page--;
+        }
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public int getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public int getPageCount() {
+        this.pageCount = (int) Math.ceil(this.getDao().count() / (double) pageSize);
+        return pageCount;
+    }
+
+    public void setPageCount(int pageCount) {
+        this.pageCount = pageCount;
+    }
+
+    public String getTitle(int id) {
         Urunler c = this.getDao().findByID(id);
         return c.getAd();
     }
-
 
     public void create() {
         this.getDao().create(entity);
@@ -69,7 +113,7 @@ public class UrunlerBean implements Serializable {
     }
 
     public List<Urunler> getList() {
-        this.list = this.getDao().getList();
+        this.list = this.getDao().getList(page, pageSize);
         return list;
     }
 
