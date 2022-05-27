326d82226e33c66653be1f5e352687de91267207
CemileGozalici <c.gozalici234423@gmail.com>
2022-05-27 15:09:30 +0300

    bean vaildetor

---
+++ src/src/java/beanValidator/TestBean.java
@@ -0,0 +1,33 @@
+package beanValidator;
+
+import jakarta.inject.Named;
+import jakarta.enterprise.context.SessionScoped;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.component.UIComponent;
+import jakarta.faces.context.FacesContext;
+import jakarta.faces.validator.ValidatorException;
+import java.io.Serializable;
+
+@Named(value = "testBean")
+@SessionScoped
+public class TestBean implements Serializable {
+
+   
+
+    public TestBean() {
+    }
+
+    public boolean validatePassword(FacesContext context, UIComponent cmp, Object value) throws ValidatorException {
+        String v = (String) value;
+        if (v.isEmpty()) {
+            throw new ValidatorException(new FacesMessage("şifre alanı boş olamaz!"));
+
+        } else if (v.length() < 3) {
+            throw new ValidatorException(new FacesMessage("şifre alanı 3 karakterden kısa olamaz!"));
+        }
+        return true;
+    }
+
+  
+
+}
