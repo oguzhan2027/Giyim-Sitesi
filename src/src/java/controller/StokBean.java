158f30c6232c0c211ca53e849f80672bbc0f2344
tugcebilici <tugcebilici123@gmail.com>
2022-05-27 15:33:10 +0300

    stok bean

--- src/src/java/controller/StokBean.java
+++ src/src/java/controller/StokBean.java
@@ -11,7 +11,6 @@ import jakarta.enterprise.context.SessionScoped;
 import java.io.Serializable;
 import java.util.List;
 
-
 @Named(value = "stokBean")
 @SessionScoped
 public class StokBean implements Serializable {
@@ -22,12 +21,55 @@ public class StokBean implements Serializable {
 
     public StokBean() {
     }
+    private int page = 1;
+    private int pageSize = 2;
+    private int pageCount;
+
+    public void next() {
+        if (this.page == this.getPageCount()) {
+            this.page = 1;
+        } else {
+            this.page++;
+        }
+    }
+
+    public void previous() {
+        if (this.page == 1) {
+            this.page = this.getPageCount();
+        } else {
+            this.page--;
+        }
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public int getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public int getPageCount() {
+        this.pageCount = (int) Math.ceil(this.getDao().count() / (double) pageSize);
+        return pageCount;
+    }
+
+    public void setPageCount(int pageCount) {
+        this.pageCount = pageCount;
+    }
 
     public int getTitle(int id) {
         Stok s = this.getDao().findByID(id);
         return s.getAdet();
     }
-
 
     public void create() {
         this.getDao().create(entity);
@@ -71,7 +113,7 @@ public class StokBean implements Serializable {
     }
 
     public List<Stok> getList() {
-        this.list = this.getDao().getList();
+        this.list = this.getDao().getList(page, pageSize);
         return list;
     }
 
