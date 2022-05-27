1e9f6a8db6b7211f7a208c61a6966722c594cd7d
CemileGozalici <c.gozalici234423@gmail.com>
2022-05-27 15:19:55 +0300

    document entity

---
+++ src/src/java/entity/Document.java
@@ -0,0 +1,42 @@
+package entity;
+
+public class Document {
+
+    private String filePath;
+    private String fileName;
+    private String fileType;
+    private Long id;
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFileType() {
+        return fileType;
+    }
+
+    public void setFileType(String fileType) {
+        this.fileType = fileType;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}
