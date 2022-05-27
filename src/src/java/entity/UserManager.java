124f57ee7041d68e938cca7b62fc04e2cded8a08
CemileGozalici <c.gozalici234423@gmail.com>
2022-05-27 16:20:23 +0300

    user manager

---
+++ src/src/java/entity/UserManager.java
@@ -0,0 +1,28 @@
+
+package entity;
+
+public class UserManager {
+    private String username;
+    private String password;
+
+    public UserManager() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+}
