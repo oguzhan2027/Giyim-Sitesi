59b45ab4bdf6ede2d2f63fc5e0a9a989c0d8a828
nesli9 <doanneslihan@gmail.com>
2022-05-27 15:50:43 +0300

    musterihizmet bean

--- src/src/java/controller/MusteriHizmetBean.java
+++ src/src/java/controller/MusteriHizmetBean.java
@@ -17,7 +17,52 @@ public class MusteriHizmetBean implements Serializable {
 
     public MusteriHizmetBean() {
     }
+  private int page = 1;
+    private int pageSize = 2;
+    private int pageCount;
 
+    public void next() {
+        if (this.page == this.getPageCount()) {
+            this.page = 1;
+        } else {
+            this.page++;
+        }
+    }
+
+    public void previous() {
+        if (this.page == 1) {
+            this.page = this.getPageCount();
+        } else {
+            this.page--;
+        }
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public int getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public int getPageCount() {
+        this.pageCount = (int) Math.ceil(this.getDao().count() / (double) pageSize);
+        return pageCount;
+    }
+
+    public void setPageCount(int pageCount) {
+        this.pageCount = pageCount;
+    }
+
+    
     public String getTitle(int id) {
         MusteriHizmet m = this.getDao().findByID(id);
         return m.getTelefon();
@@ -65,7 +110,7 @@ public class MusteriHizmetBean implements Serializable {
     }
 
     public List<MusteriHizmet> getList() {
-        this.list = this.getDao().getList();
+        this.list = this.getDao().getList(page, pageSize);
         return list;
     }
 
