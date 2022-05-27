93d3e4d31ab6c52828c0f22f0c026b3896446efe
tugcebilici <tugcebilici123@gmail.com>
2022-05-27 15:35:39 +0300

    urunler dao

--- src/src/java/dao/UrunlerDAO.java
+++ src/src/java/dao/UrunlerDAO.java
@@ -1,45 +1,35 @@
-
 package dao;
 
-
-import entity.Kategori;
-import entity.MusteriHizmet;
 import entity.Stok;
-import entity.Urunler;
-import java.sql.ResultSet;
 import java.sql.Statement;
+import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 import util.DBConnection;
 
-public class UrunlerDAO extends DBConnection {
+public class StokDAO extends DBConnection {
 
-    private KategoriDAO kategoriDao;
-    private StokDAO stokDao;
-    private MusteriHizmetDAO musteriHizmetDao;
-
-    public Urunler findByID(int id) {
-        Urunler u = null;
+    public Stok findByID(int id) {
+        Stok s = null;
 
         try {
             Statement st = this.getConnection().createStatement();
-            String query = "select * from urunler where id=" + id;
+            String query = "select * from stok where id=" + id;
             ResultSet rs = st.executeQuery(query);
             while (rs.next()) {
-                u = new Urunler(rs.getInt("id"), rs.getString("ad"));
+                s = new Stok(rs.getInt("id"), rs.getInt("stok_adet"));
             }
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
 
         }
-        return u;
+        return s;
     }
 
-    public void create(Urunler u) {
+    public void create(Stok s) {
         try {
             Statement st = this.getConnection().createStatement();
-
-            String query = "insert into urunler (kategori_id,ad,fiyat,stok_id,musteri_hizmet_id) values (" + u.getKategori().getId() + ",'" + u.getAd() + "','" + u.getFiyat() + "','" + u.getStok().getId() + "','" + u.getMusteriHizmet().getId() + "')";
+            String query = "insert into stok (stok_adet) values ('" + s.getAdet() + "')";
             st.executeUpdate(query);
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
@@ -47,10 +37,10 @@ public class UrunlerDAO extends DBConnection {
         }
     }
 
-    public void update(Urunler u) {
+    public void update(Stok s) {
         try {
             Statement st = this.getConnection().createStatement();
-            String query = "update urunler set kategori_id=" + u.getKategori().getId() + ",ad='" + u.getAd() + "',fiyat='" + u.getFiyat() + "',stok_id='" + u.getStok().getId() + "',musteri_hizmet_id='" + u.getMusteriHizmet().getId() + "'where id=" + u.getId();
+            String query = "update stok set stok_adet='" + s.getAdet() + "'where id=" + s.getId();
             st.executeUpdate(query);
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
@@ -58,10 +48,10 @@ public class UrunlerDAO extends DBConnection {
         }
     }
 
-    public void delete(Urunler u) {
+    public void delete(Stok s) {
         try {
             Statement st = this.getConnection().createStatement();
-            String query = "delete from urunler where id=" + u.getId();
+            String query = "delete from stok where id=" + s.getId();
             st.executeUpdate(query);
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
@@ -69,56 +59,36 @@ public class UrunlerDAO extends DBConnection {
         }
     }
 
-    public List<Urunler> getList() {
-        List<Urunler> list = new ArrayList<>();
+    public int count() {
+        int count = 0;
         try {
             Statement st = this.getConnection().createStatement();
-            String query = "select * from urunler";
+            String query = "select count(id) as stok_count from stok";
+            ResultSet rs = st.executeQuery(query);
+            rs.next();
+            count = rs.getInt("stok_count");
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+
+        }
+        return count;
+    }
+
+    public List<Stok> getList(int page, int pageSize) {
+        List<Stok> list = new ArrayList<>();
+        int start = (page - 1) * pageSize;
+        try {
+            Statement st = this.getConnection().createStatement();
+            String query = "select * from stok order by id asc limit " + pageSize + " offset " + start;
             ResultSet rs = st.executeQuery(query);
             while (rs.next()) {
-                Kategori k = this.getKategoriDao().findByID(rs.getInt("kategori_id"));
-                Stok s = this.getStokDao().findByID(rs.getInt("stok_id"));
-                MusteriHizmet m = this.getMusteriHizmetDao().findByID(rs.getInt("musteri_hizmet_id"));
-                list.add(new Urunler(rs.getInt("id"), k, rs.getString("ad"), rs.getInt("fiyat"), s, m));
+                list.add(new Stok(rs.getInt("id"), rs.getInt("stok_adet")));
             }
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
 
         }
         return list;
-    }
-
-    public KategoriDAO getKategoriDao() {
-        if (kategoriDao == null) {
-            this.kategoriDao = new KategoriDAO();
-        }
-        return kategoriDao;
-    }
-
-    public void setKategoriDao(KategoriDAO kategoriDao) {
-        this.kategoriDao = kategoriDao;
-    }
-
-    public StokDAO getStokDao() {
-        if (stokDao == null) {
-            this.stokDao = new StokDAO();
-        }
-        return stokDao;
-    }
-
-    public void setStokDao(StokDAO stokDao) {
-        this.stokDao = stokDao;
-    }
-
-    public MusteriHizmetDAO getMusteriHizmetDao() {
-        if (musteriHizmetDao == null) {
-            this.musteriHizmetDao = new MusteriHizmetDAO();
-        }
-        return musteriHizmetDao;
-    }
-
-    public void setMusteriHizmetDao(MusteriHizmetDAO musteriHizmetDao) {
-        this.musteriHizmetDao = musteriHizmetDao;
     }
 
 }
